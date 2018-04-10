51564942084a11c58b3ff41cc62eb6468ed4848d
Devin Parkinson <dparky13@icloud.com>
2018-04-10 11:47:08 -0600

    inital commit

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,13 @@
+class Api::MessagesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    email = params[:email]
+    body = params[:body]
+    message = Message.new(email: email, body: body)
+    if message.save
+    MessageBus.publish "/chat_channel", message.to_json
+    else
+      render json: { errors: message.errors.full_messages }, status: 422
+  end
+end
